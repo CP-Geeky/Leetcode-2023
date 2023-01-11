47d9e2d3dc6d3d202eb6adc781c3ede10ab6594d
CP_geeky <pavanpatel9320@gmail.com>
2023-01-11 19:12:03 +0530

    11-January-2023

---
+++ 11_minTimeToCollectAllApples.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> adjList;
+    int dfs(vector<bool>& hasApple,int node,int d,int prev)
+    {
+        int result=0,temp;
+        for(int &i:adjList[node])
+	    if(i!=prev)
+	    {
+	        temp=dfs(hasApple,i,d+1,node);
+	        if(temp) result+=temp-d;
+	    }
+        return result||hasApple[node]?result+d:0; 
+        
+    }
+    int minTime(int n, vector<vector<int>>& edges, vector<bool>& hasApple) 
+    {
+        adjList.resize(n);
+        for(vector<int> &e:edges)
+            adjList[e[0]].push_back(e[1]),adjList[e[1]].push_back(e[0]);
+        return dfs(hasApple,0,0,-1)*2;
+    }
+};
+
+int main(){
+    int n = 7;
+    vector<vector<int>> edges = {{0, 1}, {0, 2}, {1, 4}, {1, 5}, {2, 3}, {2,6}};
+    vector<bool> hasApple = {false, false, false, false, false, false, false};
+    Solution obj;
+    int minApples = obj.minTime(n, edges, hasApples);
+    cout << "Minimum Apples required : " << minApples << endl; 
+
+}
